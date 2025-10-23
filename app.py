f3ce5ac791eca82ecec6a6ff9b05201feb0fc232
Sunny Gogoi <sgogoi2004@gmail.com>
2025-10-23 12:25:05 +0530

    Prod ready Flask

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def chat():
     return str(response.content)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port= 8080, debug= False)
